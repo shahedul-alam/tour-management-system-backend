457818ade6cfb9218569b601f5e6d012a295af06
shahedul-alam <shaheedalamcontact@gmail.com>
2025-10-31 23:25:09 +0600

    invoice download api added

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -181,9 +181,24 @@ const cancelPayment = async (query: Record<string, string>) => {
   }
 };
 
+const getInvoiceDownloadUrl = async (paymentId: string) => {
+  const payment = await Payment.findById(paymentId).select("invoiceUrl");
+
+  if (!payment) {
+    throw new AppError(401, "Payment not found");
+  }
+
+  if (!payment.invoiceUrl) {
+    throw new AppError(401, "Invoice not available yet");
+  }
+
+  return payment.invoiceUrl;
+};
+
 export const PaymentService = {
   initPayment,
   successPayment,
   failPayment,
   cancelPayment,
+  getInvoiceDownloadUrl,
 };
